d1a4a07c219325aaaa447aadb6965385dc2c2298
cotitan <cotitan@outlook.com>
2019-01-17 00:49:39 +0800

    Transformer implementation, not finish yet

--- Transformer.py
+++ Transformer.py
@@ -25,9 +25,23 @@ class ScaledDotAttention(nn.Module):
         return attn_vec, attn_weight
 
 
+class LayerNorm(nn.Module):
+    def __init__(self, hidden_size, eps=1e-12):
+        super(LayerNorm, self).__init__()
+        self.scale = nn.Parameter(torch.ones(hidden_size))
+        self.shift = nn.Parameter(torch.zeros(hidden_size))
+        self.eps = eps
+    
+    def forward(self, x):
+        mu = x.mean(-1, keepdim=True)
+        sigma = (x - mu).powe(2).mean(-1, keep_dim=True)
+        x = (x - mu) / torch.sqrt(sigma + self.eps) # avoid sqrt(0)
+        return self.scale * x + self.shift
+
+
 class Embedding(nn.Module):
     def __init__(self, config, embeddings=None):
-        super(Embedding).__init__()
+        super(Embedding, self).__init__()
 
         if embeddings is not None:
             self.word_embeddings = nn.Embedding.from_pretrained(embeddings)
@@ -39,5 +53,62 @@ class Embedding(nn.Module):
 
         # self.LayerNorm is not snake-cased to stick with TensorFlow model variable name and be able to load
         # any TensorFlow checkpoint file
-        self.LayerNorm = BertLayerNorm(config.hidden_size, eps=1e-12)
+        self.LayerNorm = LayerNorm(config.hidden_size, eps=1e-12)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
+    
+    def forward(self, input_ids):
+        pass  # TODO
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_head=8, d_model=512, d_q=64, d_k=64, d_v=64):
+        super(MultiHeadAttention, self).__init__()
+        assert d_q == d_k, "dim(key) must be equal to dim(query)"
+
+        self.num_head = num_head
+
+        self.W_Q = [nn.Linear(d_q, d_model) for _ in range(num_head)]
+        self.W_K = [nn.Linear(d_k, d_model) for _ in range(num_head)]
+        self.W_V = [nn.Linear(d_v, d_model) for _ in range(num_head)]
+
+        self.attn_layer = ScaledDotAttention()
+        self.linear_out = nn.Linear(d_model, d_model)
+    
+    def forward(self, q, k, v):
+        Qs = [self.W_Q(q) for _ in range(self.num_head)]
+        Ks = [self.W_K(k) for _ in range(self.num_head)]
+        Vs = [self.W_V(v) for _ in range(self.num_head)]
+        heads = [self.attn_layer(Qs[i], Ks[i], Vs[i]) for i in range(self.num_head)]
+        out = self.linear_out(torch.cat(heads))
+        return out
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        # TODO
+        pass
+    
+    def forward(nn.Module):
+        # TODO
+        pass
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super(Decoder, self).__init__()
+        # TODO
+        pass
+    
+    def forward(self):
+        # TODO
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+        # TODO
+    
+    def forward(self):
+        # TODO
+
